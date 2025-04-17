88fe716c4f5ccdda8293c029999d26b13b7b0694
ShirYahav <shiryahav07@gmail.com>
2025-04-17 15:46:11 +0300

    updated create and update preferences so it will works with enums , only brands are from DB

--- backend/src/models/preferences_model.py
+++ backend/src/models/preferences_model.py
@@ -1,11 +1,15 @@
 import mongoengine as me
 
+from src.enums.aesthetics_enum import AestheticsEnum
+from src.enums.celebrities_enum import CelebritiesEnum
+from src.enums.colors_enum import ColorsEnum
+
 class Preferences(me.Document):
     user = me.ReferenceField("User", required=True)
     brands = me.ListField(me.StringField(), default=list)
-    aesthetics = me.ListField(me.StringField(), default=list)
-    celebrities = me.ListField(me.StringField(), default=list)
-    colors = me.ListField(me.StringField(), default=list)
+    aesthetics = me.ListField(me.StringField(choices=[e.value for e in AestheticsEnum]), default=list)
+    celebrities = me.ListField(me.StringField(choices=[e.value for e in CelebritiesEnum]), default=list)
+    colors = me.ListField(me.StringField(choices=[e.value for e in ColorsEnum]), default=list)
 
     meta = {
         'collection': 'preferences'
